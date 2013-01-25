e365354414b361e0c7c52afb11c4043f48fd074e
Anton Mitrakhov <mitrakhov@gmail.com>
2013-01-25 19:46:34 +0200

    Revert "Merge branch 'master' of https://github.com/mitrakhov/pipeline"
    This reverts commit a349dc49af75b58c9f53c2cd1e91ac6b79e4ab14, reversing
    changes made to 4032de4954be55f0e655de4c07c070953d31b5ac.

--- software/nuke/N6/SCRIPTS/exrUnpack.py
+++ software/nuke/N6/SCRIPTS/exrUnpack.py
@@ -3,15 +3,10 @@ def exrUnpack():
     n = nuke.selectedNode()
     chList = list(set([x.split('.')[0] for x in n.channels()]))
     
-    allNames = [x['name'].value() for x in nuke.allNodes() if x.Class()=="Shuffle"]
-    
     for i in chList:
         sh = nuke.createNode('Shuffle', inpanel=False)       
         sh.knob('in').setValue(i)
-        iName = i + '.1'
-        if iName in allNames: 
-            iName = i + '.' + str(int(iName.rsplit('.',1)[-1])+1)
-        sh.knob('name').setValue(iName)
+        sh.knob('name').setValue(i)
         sh.setInput(0, n)
         sh.knob('hide_input').setValue(True)
         sh.knob('postage_stamp').setValue(True)
