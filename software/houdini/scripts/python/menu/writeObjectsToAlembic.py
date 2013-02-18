cfabee35545096c57f5c978d3bbcc5b615c016c1
stormic <skrypnyk.vlad@gmail.com>
2013-02-18 13:30:57 +0200

    update scripts/menu

--- software/houdini/scripts/python/menu/writeObjectsToAlembic.py
+++ software/houdini/scripts/python/menu/writeObjectsToAlembic.py
@@ -1,5 +1,9 @@
 import cache_utils
 reload(cache_utils)
 
-i, buff = hou.ui.readMultiInput("Input framerange and increment", ("Start","End", "Increment"),title="Write Objects To Alembic") 
+#
+rfstart = int(hou.playbar.playbackRange()[0])
+rfend = int(hou.playbar.playbackRange()[1])
+
+i, buff = hou.ui.readMultiInput("Input framerange and increment", ("Start","End", "Samples"),title="Write Objects To Alembic", initial_contents = ( str(rfstart), str(rfend), '1' ) )  
 cache_utils.abcCacheWrite(int(buff[0]),int(buff[1]), int(buff[2]) )
