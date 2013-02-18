cd5623cf32c820c6dbb68b09c3d93c9a70d62035
stormic <skrypnyk.vlad@gmail.com>
2013-02-18 12:47:09 +0200

    update houdini part

--- software/houdini/scripts/python/menu/writeObjectsToAlembic.py
+++ software/houdini/scripts/python/menu/writeObjectsToAlembic.py
@@ -1,4 +1,5 @@
-import utils
-reload(utils)
-i, buff = hou.ui.readMultiInput("Input framerange", ("Start","End"),title="Bake object to world") 
-utils.bakeObjectToWorld(int(buff[0]),int(buff[1]))
+import cache_utils
+reload(cache_utils)
+
+i, buff = hou.ui.readMultiInput("Input framerange and increment", ("Start","End", "Increment"),title="Write Objects To Alembic") 
+cache_utils.abcCacheWrite(int(buff[0]),int(buff[1]), int(buff[2]) )
