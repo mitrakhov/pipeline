5d1d0868aa3e7b9a308483e92e4c420da9252c65
stormic <skrypnyk.vlad@gmail.com>
2013-02-22 13:27:29 +0200

    add cancel to the writeToObjectsToAlembic.py

--- software/houdini/scripts/python/menu/writeObjectsToAlembic.py
+++ software/houdini/scripts/python/menu/writeObjectsToAlembic.py
@@ -4,5 +4,5 @@ reload(cache_utils)
 rfstart = int(hou.playbar.playbackRange()[0])
 rfend = int(hou.playbar.playbackRange()[1])
 
-i, buff = hou.ui.readMultiInput("Input framerange and increment", ("Start","End", "Samples"),title="Write Objects To Alembic", initial_contents = ( str(rfstart), str(rfend), '1' ) )  
-cache_utils.abcCacheWrite(int(buff[0]),int(buff[1]), int(buff[2]) )
+i, buff = hou.ui.readMultiInput("Input framerange and increment", ("Start","End", "Samples"),title="Write Objects To Alembic",buttons = ('Ok', 'Cancel'), default_choice = 0, close_choice = 1, initial_contents = ( str(rfstart), str(rfend), '1' ) )  
+if i ==  0: cache_utils.abcCacheWrite(int(buff[0]),int(buff[1]), int(buff[2]) )
