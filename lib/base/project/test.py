1efdc6654446a77a9b7f9488cc62c25545a268e6
Anton Mitrakhov <mitrakhov@gmail.com>
2013-02-28 16:49:43 +0200

    finding the ways to kill users

--- lib/base/project/test.py
+++ lib/base/project/test.py
@@ -2,14 +2,19 @@
 from shotgun_api3 import Shotgun
 import os
 
-"""
+
 site = 'https://chimneypot.shotgunstudio.com'    
 scriptName = 'dBase'
 scriptKey = '729a76955455909c79f6d90262bb9fbe9186b92b'
 pName = 'kievPipelineTest'
-sg = ShotgunDB(site, scriptName, scriptKey)
-"""
-sg = os.listdir('/tmp')
+sg = Shotgun(site, scriptName, scriptKey)
+
+lst = sg.find('HumanUser', [['updated_at', 'in_last', 1, 'DAY'],['sg_status_list', 'is', 'act']], ['name', 'updated_at', 'sg_status_list'])
+for i in lst:
+	print "%s: %s, %s, %s"%(i['name'],i['updated_at'], i['sg_status_list'], i['id'])
+
+killEric = sg.update('HumanUser', 62, {'sg_status_list':'dis'})
+
 
 def test():
 	print 'huy'    
