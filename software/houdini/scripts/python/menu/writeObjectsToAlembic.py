9733f15f609ce2e2d120e6107fbd7b32a5753b33
stormic <skrypnyk.vlad@gmail.com>
2013-03-18 10:55:07 +0200

    add overwrite mode to cache_utils

--- software/houdini/scripts/python/menu/writeObjectsToAlembic.py
+++ software/houdini/scripts/python/menu/writeObjectsToAlembic.py
@@ -4,5 +4,6 @@ reload(cache_utils)
 rfstart = int(hou.playbar.playbackRange()[0])
 rfend = int(hou.playbar.playbackRange()[1])
 
-i, buff = hou.ui.readMultiInput("Input framerange and increment", ("Start","End", "Samples"),title="Write Objects To Alembic",buttons = ('Ok', 'Cancel'), default_choice = 0, close_choice = 1, initial_contents = ( str(rfstart), str(rfend), '1' ) )  
-if i ==  0: cache_utils.abcCacheWrite(int(buff[0]),int(buff[1]), int(buff[2]) )
+i, buff = hou.ui.readMultiInput("Input framerange and increment", ("Start","End", "Samples"),title="Write Objects To Alembic",buttons = ('Version', 'Overwrite', 'Cancel'), default_choice = 0, close_choice = 2, initial_contents = ( str(rfstart), str(rfend), '1' ) )  
+if i ==  0: cache_utils.abcCacheWrite(int(buff[0]),int(buff[1]), int(buff[2]), 0 )
+if i ==  1: cache_utils.abcCacheWrite(int(buff[0]),int(buff[1]), int(buff[2]), 1 )
