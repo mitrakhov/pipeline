58f9e42e9588f4d4c08d50b469b09d231ce775bd
mitrakhov <mitrakhov@gmail.com>
2013-03-25 13:48:29 +0200

    'rat' extension added

--- software/nuke/N6/SCRIPTS/init.py
+++ software/nuke/N6/SCRIPTS/init.py
@@ -1,15 +1,12 @@
  
 
 nuke.knobDefault("fps", "25")
-nuke.knobDefault("format", "720 576 0 0 720 576 1.067 PAL_1.067")
-#nuke.knobDefault("format", "1920 1080 0 0 1920 1080 1 HD")
-#nuke.knobDefault("proxy_type", "format")
+nuke.knobDefault("format", "1920 1080 0 0 1920 1080 1 HD")
+nuke.knobDefault("proxy_type", "format")
 nuke.knobDefault("before", "black")
 nuke.knobDefault("after", "black")
 nuke.knobDefault("on_error", "checkerboard")
-#sys.path.append("//Bigboy/D/tfxtools/Nuke/python")
-#sys.path.append("/mnt/tfxtools/Nuke/python")
-#sys.path.append("/Volumes/d/tfxtools/Nuke/python")
+nuke.addSequenceFileExtension("rat")
 
 #nuke.knobDefault("Root.monitorLut", "linear")
 #nuke.knobDefault("Root.viewerLut", "linear")
