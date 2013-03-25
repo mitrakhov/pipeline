b49b9b385137c8a96db2be306884b1f6eaaa81cf
mitrakhov <mitrakhov@gmail.com>
2013-03-25 13:57:59 +0200

    pic ext added

--- software/nuke/N6/SCRIPTS/init.py
+++ software/nuke/N6/SCRIPTS/init.py
@@ -7,6 +7,7 @@ nuke.knobDefault("before", "black")
 nuke.knobDefault("after", "black")
 nuke.knobDefault("on_error", "checkerboard")
 nuke.addSequenceFileExtension("rat")
+nuke.addSequenceFileExtension("pic")
 
 #nuke.knobDefault("Root.monitorLut", "linear")
 #nuke.knobDefault("Root.viewerLut", "linear")
