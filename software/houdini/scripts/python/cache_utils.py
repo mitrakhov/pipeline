3a5592061bb327db0fa4d51c9d65c1c1cc95c2e9
stormic <skrypnyk.vlad@gmail.com>
2013-04-04 01:19:14 +0300

    Update cache_utils.py
    optimize script

--- software/houdini/scripts/python/cache_utils.py
+++ software/houdini/scripts/python/cache_utils.py
@@ -1,23 +1,27 @@
 import os
 import hou
 
-def makeCachePath(mode, object):
+def makeCachePath(object, mode):
+    #get all path from HOUDINI VARIABLES
+
     sceneName = hou.getenv('HIPNAME').rsplit('.v',1)[0]
     dataPath = hou.getenv('DATA')
-    geoPath = dataPath + "/geo"
-    scenePath = geoPath + "/" + sceneName
-    nodePath = scenePath + "/" + object.name()
+    geoPath = os.path.join(dataPath, "geo")
+    scenePath = os.path.join(geoPath, sceneName)
+    nodePath = os.path.join(scenePath, object.name())
+
     startVersion = 0
     
-    #mode 0 - get cache info
-    if mode == 0:
+    #get mode - get cache info
+    if mode == 'get':
         cachesList = []
         if os.path.exists(nodePath):
             for n in sorted(os.listdir(nodePath)):
-                cachesList.append(nodePath + '/' + n)
+                cachesList.append(os.path.join(nodePath, n))
             return cachesList
-        
-    if mode == 2:
+    
+    #add mode - add new cache path
+    if mode == 'add':
         versionPath = ''
         #mkdir cache for current scene
         if not os.path.exists(scenePath): os.mkdir(scenePath)
@@ -25,12 +29,12 @@ def makeCachePath(mode, object):
         if not os.path.exists(nodePath): os.mkdir(nodePath)
         
         if not os.listdir(nodePath):
-            versionPath = nodePath + "/" + str(startVersion).zfill(4)
+            versionPath = os.path.join(nodePath, str(startVersion).zfill(3))
             os.mkdir(versionPath)
         else:
             currentVersion = int(max(os.listdir(nodePath)))
-            if os.path.exists(nodePath + "/" + str(startVersion).zfill(4)):
-                versionPath = nodePath + "/" + str(currentVersion + 1).zfill(4)
+            if os.path.exists(os.path.join(nodePath, str(startVersion).zfill(3))):
+                versionPath = os.path.join(nodePath, str(currentVersion + 1).zfill(3))
                 os.mkdir(versionPath)
         return versionPath
 
@@ -44,10 +48,10 @@ def abcCacheWrite(startFrame, endFrame, subFrame, mode):
     message = ''
     for n in selectedNodes:
 
-        if mode == 0:
-            cachePath = makeCachePath(2, n)
+        if mode == 'add':
+            cachePath = makeCachePath(n, 'add')
         else:
-            cachePath = makeCachePath(0, n)[-1]
+            cachePath = makeCachePath(n, 'get')[-1]
 
         message += 'NODE' + ' - ' + n.path() + ' > ' + 'CACHE - $DATA/geo/' + cachePath.split('geo/')[1] + '/' + n.name() + '.abc' + '\n'
         alembic = hou.node(ropnet.path()).createNode("alembic")
