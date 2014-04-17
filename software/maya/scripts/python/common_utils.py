ea27625ba9cce811f85035c50b52b6b157c474b8
stormic <skrypnyk.vlad@gmail.com>
2014-04-18 01:56:37 +0300

    Update common_utils.py

--- software/maya/scripts/python/common_utils.py
+++ software/maya/scripts/python/common_utils.py
@@ -16,3 +16,33 @@ def bakeObjectAnimToWorld(object, startFrame, endFrame):
         orientCon = orientConstraint(object, camBake)
         bakeResults(camBake, attribute = ('translate', 'rotate'), time = (startFrame, endFrame) )
         delete(pointCon, orientCon)
+        
+
+def assignWarningShader(node):
+    surface = createNode('lambert', name = node.name() + '_Surface')
+    surface.color.set([1, 0, 0])
+    shader = sets( renderable=True, noSurfaceShader=True, empty=True, name=node.name() + '_Shader' )
+    connectAttr(surface.name() + '.outColor', shader.name() + '.surfaceShader')
+    sets(shader.name(),  forceElement = node.name())
+
+def analyzeStruct(rootNode):
+    if not rootNode.getShape() and not rootNode.getParent():
+        struct = rootNode.listRelatives(allDescendents = True, type = 'transform')
+        for node in struct:
+            nodeNameIdent = node.name().split('_')
+            #test group node
+            if not node.getShape() and len(nodeNameIdent) == 2:
+                if not nodeNameIdent[0] == 'g':
+                        select(node)
+                        warning(''.join(['[', node.name(), ']', ' - ', 'Transform have wrong prefix']) )
+            elif len(nodeNameIdent) == 4:
+                print 'a', node.name()
+                sets('initialShadingGroup',  forceElement = node.name())
+            else:
+                assignWarningShader(node)
+                    
+    else:
+        warning('It not Root Node')
+
+#for x in ls( type='transform'):
+analyzeStruct(selected()[0])
